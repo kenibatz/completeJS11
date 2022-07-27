d04f7e26f7a97d0ccf6178078bfdb49a009a16bf
kenibatz <kenny.batallones@gmail.com>
2022-07-27 20:39:48 +0800

    map methods and arrow functions

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 'use strict';
 
 console.log(`\n*** Data Transormations with Map, Filter and Reduce`);
+
+console.log(
+  `\n*** Map creates a new array and applies an operation on each element`
+);
+
+const monthlyExpensesUSD = [10, 11, 10, 5, 8];
+
+console.log(`monthly expenses USD: ${monthlyExpensesUSD}`);
+
+const conversionRate = 50;
+
+//regular function declaration
+/*
+const monthlyExpenses = monthlyExpensesUSD.map(function (expense, index, array) {
+  return expense * conversionRate;
+});
+*/
+
+//arrow function declaration
+const monthlyExpenses = monthlyExpensesUSD.map(
+  expense => expense * conversionRate
+);
+
+//arrow function 2
+//example of the arrow function expanded
+/*
+const monthlyExpenses = monthlyExpensesUSD.map((expense, index) => {
+  console.log(`arrow function 2 `);
+  return `${index} : ${expense * conversionRate}`;
+});
+*/
+
+console.log(`monthly expenses converted: ${monthlyExpenses}`);
